6e128ddf5e0ba78a4c5a1b2f661d82609d0dbc0f
Jack Smith <jackjoshuasmith@outlook.com>
2022-04-19 13:25:18 +0100

    Moved bracket

--- app.py
+++ app.py
@@ -50,5 +50,5 @@ def work():
 
 # Run flask
 if __name__ == '__main__':
-    port = int(environ.get('PORT'), 5000)
+    port = int(environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True) 
